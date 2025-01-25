7ff3cd2188a439afb3a643a11fe2ad13d8970bb6
SuSSan@143 <budtut143@gmail.com>
2025-01-25 16:13:05 +0530

    added entries for linux process blog

--- src/routes/portfolio/blogs/constants.ts
+++ src/routes/portfolio/blogs/constants.ts
@@ -11,8 +11,17 @@ import testing004 from '$lib/images/testing004.webp';
 import testing005 from '$lib/images/testing005.jpeg';
 import testing006 from '$lib/images/testing006.jpeg';
 import testing001 from '$lib/images/ansible001.jpeg';
+import linuxProcesses001 from '$lib/images/linux-processes001.webp';
 
 export const blogs = [
+	{
+		name: 'The Linux Process Party: How to Kill, Fork, and Keep It All Running Smoothly',
+
+		img: linuxProcesses001,
+		webkitURL:
+			'https://dev.to/sandheep_kumarpatro_1c48/the-linux-process-party-how-to-kill-fork-and-keep-it-all-running-smoothly-5gmn',
+		tags: ['linux', 'process', 'kill', 'fork', 'daemon']
+	},
 	{
 		name: "From 'A' to '😊': How Programming Languages Handle Strings",
 		img: string001,
@@ -25,79 +34,79 @@ export const blogs = [
 		img: react001,
 		webkitURL:
 			'https://dev.to/sandheep_kumarpatro_1c48/react-component-communication-parent-child-and-child-parent-interactions-38og',
-		tags: ['programming', 'encoding', 'unicode', 'asci']
+		tags: ['react', 'communication', 'parent-child', 'child-parent']
 	},
 	{
 		name: 'Ditch the GUI: How to Master Android Emulators with Just the CLI Like a True Terminal Nerd',
 		img: emulator001,
 		webkitURL:
 			'https://dev.to/sandheep_kumarpatro_1c48/ditch-the-gui-how-to-master-android-emulators-with-just-the-cli-like-a-true-terminal-nerd-3a83',
-		tags: ['programming', 'encoding', 'unicode', 'asci']
+		tags: ['android', 'emulator', 'cli', 'terminal', 'nerd', 'power-user']
 	},
 	{
 		name: '3. Beyond the Basics: In-Depth Look at Linux Display Servers, Window Managers, and Compositors',
 		img: linux001,
 		webkitURL:
 			'https://dev.to/sandheep_kumarpatro_1c48/beyond-the-basics-in-depth-look-at-linux-display-servers-window-managers-and-compositors-40bk',
-		tags: ['programming', 'encoding', 'unicode', 'asci']
+		tags: ['linux', 'gui', 'display', 'window', 'manager', 'compositor']
 	},
 	{
 		name: '2. The Ultimate Guide to Linux GUI: Systems Understanding the Core Components and Comparisons',
 		img: linux002,
 		webkitURL:
 			'https://dev.to/sandheep_kumarpatro_1c48/the-ultimate-guide-to-linux-gui-systems-understanding-the-core-components-and-comparisons-2il5',
-		tags: ['programming', 'encoding', 'unicode', 'asci']
+		tags: ['linux', 'gui', 'display', 'window', 'manager', 'compositor']
 	},
 	{
 		name: '1. Introduction to Linux GUIs: Unpacking the Basics of Desktop Environments, Window Managers, and More',
 		img: linux003,
 		webkitURL:
 			'https://dev.to/sandheep_kumarpatro_1c48/introduction-to-linux-guis-unpacking-the-basics-of-desktop-environments-window-managers-and-more-4anf',
-		tags: ['programming', 'encoding', 'unicode', 'asci']
+		tags: ['linux', 'gui', 'display', 'window', 'manager', 'compositor']
 	},
 	{
 		name: '5. Where to Go Now: Putting Unit Testing into Action',
 		img: testing002,
 		webkitURL:
 			'https://dev.to/sandheep_kumarpatro_1c48/5-where-to-go-now-putting-unit-testing-into-action-58el',
-		tags: ['programming', 'encoding', 'unicode', 'asci']
+		tags: ['unit-testing', 'javascript', 'vitest']
 	},
 	{
 		name: '4. Some More Examples and Explanations',
 		img: testing003,
 		webkitURL: 'https://dev.to/sandheep_kumarpatro_1c48/4-some-more-examples-and-explanations-38a3',
-		tags: ['programming', 'encoding', 'unicode', 'asci']
+		tags: ['unit-testing', 'javascript', 'vitest']
 	},
 	{
 		name: '3. Unit testing concept',
 		img: testing006,
 		webkitURL: 'https://dev.to/sandheep_kumarpatro_1c48/3-unit-testing-concept-4gj8',
-		tags: ['programming', 'encoding', 'unicode', 'asci']
+		tags: ['unit-testing', 'javascript', 'vitest']
 	},
 	{
 		name: '2. Basics of Vitest',
 		img: testing005,
 		webkitURL: 'https://dev.to/sandheep_kumarpatro_1c48/2-basics-of-vitest-50jj',
-		tags: ['programming', 'encoding', 'unicode', 'asci']
+		tags: ['unit-testing', 'javascript', 'vitest']
 	},
 	{
 		name: '1. Understanding unit test basics',
 		img: testing001,
 		webkitURL: 'https://dev.to/sandheep_kumarpatro_1c48/1-understanding-unit-test-basics-3c0m',
-		tags: ['programming', 'encoding', 'unicode', 'asci']
+		tags: ['unit-testing', 'javascript', 'vitest']
 	},
 	{
 		name: '0. Introduction and Surface level Explanation',
 		img: testing004,
 		webkitURL:
 			'hhttps://dev.to/sandheep_kumarpatro_1c48/0-introduction-and-surface-level-explanation-1gomttps://dev.to/sandheep_kumarpatro_1c48/from-a-to-how-programming-languages-handle-strings-5d22',
-		tags: ['programming', 'encoding', 'unicode', 'asci']
+		tags: ['unit-testing', 'javascript', 'vitest']
 	},
 	{
 		name: 'From Manual Drudgery to Automation Maestro: Mastering Ansible with Docker',
 		img: ansible001,
 		webkitURL:
 			'https://dev.to/sandheep_kumarpatro_1c48/from-manual-drudgery-to-automation-maestro-mastering-ansible-with-docker-10m5',
-		tags: ['programming', 'encoding', 'unicode', 'asci']
+		tags: ['ansible', 'automation', 'docker', 'docker-compose', 'dockerfile']
 	}
 ];
